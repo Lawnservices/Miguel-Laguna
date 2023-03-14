201ed9b65bfe265f8fe2dc0520ef79760f855f80
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-03-13 20:35:06 -0400

    fix

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('#galeria').click(function() {
         $('#orario').slideToggle('slow');
+        $('#menos').slideToggle('slow');
     });
 });
+
+var mesa = "Visitanos en Nuestro canal de Youtube.";
+
+function mensage() {
+    var el = document.getElementById('menos');
+    el.textContent = mesa;
+}
+mensage();
