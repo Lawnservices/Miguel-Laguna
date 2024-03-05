6da669fc25cb8cb33198278d348ef9a31135fb8e
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2024-03-05 09:27:10 -0500

    Update app.js
    fix

--- app.js
+++ app.js
@@ -12,3 +12,19 @@ function mensage() {
     el.textContent = mesa;
 }
 mensage();
+
+function color(){
+    document.getElementById("cuerpo").style.backgroundColor = "#333";
+    document.getElementById("boton1").style.display = "none";
+    document.getElementById("boton2").style.backgroundColor = "#333";
+    document.getElementById("boton2").style.display = "block";
+    document.getElementById("work").style.backgroundColor = "#003349";
+    document.getElementById("text").style.color = "#fff";
+}
+function colores(){
+    document.getElementById("cuerpo").style.backgroundColor = "#fff";
+    document.getElementById("boton1").style.display = "block";
+    document.getElementById("boton2").style.display = "none";
+    document.getElementById("work").style.backgroundColor = "#fff";
+    document.getElementById("text").style.color = "#000";
+}
