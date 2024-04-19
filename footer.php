7eb4f2087d5d4b3f84e299b385e5ff7049eec128
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-04-19 14:50:35 -0400

    fix-web

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="piedepagina">
+    <span>Laguna Lawn Services</span>
+    <span>5624 Whitfield Chapel <br>Lanham md 20706</span>
+    <span><a href="tel:12408838479">(240) 883-8479</a></span>
+    <span>Hours: M_F 7:00-4:00</span>
+    <p>©2024 Laguna Lawn Services. All Rights Reserved.</p>
+  </div>
+ 
+ 
