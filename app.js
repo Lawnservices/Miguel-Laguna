b1a5d879bf3c7d49f6b327742c3ad09b2922a8f5
miguel <80590513+Lawnservices@users.noreply.github.com>
2024-04-26 13:13:11 -0400

    fix-web

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
 window.addEventListener('load',function(){
-var imagenes =[];
-imagenes[0] = 'img/01.jpg';
-imagenes[1] = 'img/02.jpg';
-imagenes[2] = 'img/03.jpg';
-imagenes[3] = 'img/04.jpg';
-imagenes[4] = 'img/05.jpg';
-imagenes[5] = 'img/06.jpg';
-var indiceImagenes = 1;
-var time = 2000;
-function cambiarImagenes(){
-  document.slider.src = imagenes[indiceImagenes];
-
-if(indiceImagenes < 5){
-  indiceImagenes++;
-}  else{
-    indiceImagenes = 0;
-  }
-}
-setInterval(cambiarImagenes,time);
-
-document.getElementById('video').addEventListener('click',function(){
+  document.getElementById('video').addEventListener('click',function(){
   document.getElementById('video').style.display = 'none';
   document.getElementById('videos').style.display = 'block';
   document.getElementById('slider').style.display = 'none';
@@ -30,5 +10,18 @@ this.document.getElementById('videoo').addEventListener('click',function(){
   document.getElementById('videoo').style.display = 'none';
   document.getElementById('videos').style.display = 'none';
   document.getElementById('slider').style.display = 'block';
+});
+
+const caveza = document.getElementById('video');
+const cavezaa = document.getElementById('videoo');
+document.addEventListener('scroll',() => {
+ let = scroll_position = window.scrollY;
+ if(scroll_position > 360){
+  caveza.style.color = 'green';
+  cavezaa.style.color = 'yellow';
+ } else{
+  caveza.style.color = '#fff';
+  cavezaa.style.color = '#fff';
+ }
 });
 });
