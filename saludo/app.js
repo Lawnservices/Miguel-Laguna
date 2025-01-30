fa23989f97afbe5b9da60e25fd2e2f47d3656e3a
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-01-30 13:15:39 -0500

    fix-logo

---
+++ saludo/app.js
@@ -0,0 +1,14 @@
+var today = new Date();
+var todayHours = today.getHours();
+var greetin;
+
+if (todayHours >= 18){
+ greetin = 'Good evening';
+} else if (todayHours >= 12){
+ greetin = 'Good afternoon';
+} else if (todayHours >= 0){
+ greetin = 'Good morning';
+} else{
+ greetin = 'Welcome, thank you for visiting us.';
+}
+document.getElementById('demo').textContent = greetin;
