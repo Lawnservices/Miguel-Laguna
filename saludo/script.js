4375eeac810caa158da1ad5dd7c8ab2a8b316341
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-03-08 14:33:26 -0500

    fix

--- saludo/script.js
+++ saludo/script.js
@@ -1,1 +1,7 @@
 document.getElementById('year').textContent = new Date () .getFullYear();
+
+setInterval(()=>{
+ const masa = document.getElementsByClassName('logo');
+ masa[0].style.color = 'green';
+},6000);
+ 
