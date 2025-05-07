313ac116a7013b1b7ff851e1ecadd83d349e1143
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-05-07 12:53:45 -0400

    fix-web

--- saludo/app.js
+++ saludo/app.js
@@ -1,18 +1,23 @@
 var today = new Date();
 var todayHours = today.getHours();
 var greetin;
-
+let mensaje = 'We have the best prices';
 if (todayHours >= 18){
- greetin = 'Good evening ';
+ greetin = 'Good Evening ';
 } else if (todayHours >= 12){
- greetin = 'Good afternoon ';
+ greetin = 'Good Afternoon ';
 } else if (todayHours >= 0){
- greetin = 'Good morning ';
+ greetin = 'Good Morning ';
 } else{
  greetin = 'Welcome, thank you for visiting us.';
 }
-document.getElementById('demo').textContent = greetin;
+document.getElementById('demo').textContent = greetin + ' ' + mensaje;
 
+ 
+const dias = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const day = dias [new Date().getDay()];
+const felis = 'Happy'
+document.getElementById('usa').textContent = felis + ' ' + day;
  
  
  
