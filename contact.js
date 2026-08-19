64dacca626d11e8b806c4b4107f91109e9563525
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2026-08-19 08:01:50 -0400

    fix

--- contact.js
+++ contact.js
@@ -1,11 +1,50 @@
-document.addEventListener('DOMContentLoaded', function(){
-  document.getElementById("miFormulario").addEventListener("submit", function(event){
-      // Permite que el formulario se envíe
-      alert("Form sent successfully");
+const one =  document.getElementById('one');
+const year = new Date().getFullYear();
 
-      // Limpia los campos después de un breve retardo
-      setTimeout(function(){
-        document.getElementById("miFormulario").reset();
-      }, 1000); // 1 segundo de espera antes de limpiar los campos (opcional)
-    });
+one.textContent = `© ${year} Miguel Laguna Lawn Services. All Rights Reserved`;
+
+
+document.getElementById("name").addEventListener("input", function () {
+    this.value = this.value.replace(/[^A-Za-z\s]/g, "");
+
+    if (this.value.length < 3) {
+        this.setCustomValidity("The name must be at least 8 characters long.");
+    } 
+      else if (this.value.length > 30) {
+        this.setCustomValidity("The name is too long.");}
+    else {
+        this.setCustomValidity("");
+    }
 });
+
+
+document.getElementById("tel").addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9]/g, "");
+
+    if (this.value.length < 10) {
+        this.setCustomValidity("The number must have 10 digits.");
+    } else {
+        this.setCustomValidity("");
+    }
+});
+
+document.querySelector("form").addEventListener("submit", function () {
+    setTimeout(() => {
+        this.reset();
+    }, 100);
+});
+
+// document.getElementById("name").addEventListener("input", function () {
+//     this.value = this.value.replace(/[^A-Za-z\s]/g, "");
+//     if (this.value.length < 8) {
+//         this.setCustomValidity("");
+//     } else {
+//         this.setCustomValidity("");
+//     }
+// });
+
+
+// document.getElementById("tel").addEventListener("input", function () {
+//     this.value = this.value.replace(/[^0-9]/g, "");
+
+// });
